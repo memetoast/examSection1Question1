ea364125b361b28a950a02d63acdf12e06032167
memetoast <72702287+memetoast@users.noreply.github.com>
2022-09-22 12:39:48 -0500

    Add project files.

---
+++ Section1Question1Exam/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sum = 0;
+Console.WriteLine("Please enter the first number.");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\nPlease enter the second number.");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 == num2)
+{
+    sum = (num1 + num2) * 3;
+    Console.WriteLine("The value is " + sum);
+}
+else
+{
+    sum = num1 + num2;
+    Console.WriteLine("The value is " + sum);
+}
+
+
